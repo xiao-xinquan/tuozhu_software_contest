ef18342ed6757e44d12c725396b74f52187b4c4f
IIvyLEEee <156580929+IIvyLEEee@users.noreply.github.com>
2024-02-18 09:23:55 +0800

    Updated Mic_Input and freqControl

--- Atlandis_v0.0.1(upload)/Assets/Scripts/Mic_Input.cs
+++ Atlandis_v0.0.1(upload)/Assets/Scripts/Mic_Input.cs
@@ -7,21 +7,33 @@ public class Mic_Input : MonoBehaviour
 {
     public static float volume;
     AudioClip micRecord;
+    AudioSource m_MyAudioSource;
+
+    private float[] _audioSpectrum;
+    public static float spectrumValue { get; private set; }
 
     string device;
     // Start is called before the first frame update
     void Start()
     {
         device = Microphone.devices[0];
+        m_MyAudioSource = GetComponent<AudioSource>();
+        device = Microphone.devices[0];
+        m_MyAudioSource.clip = Microphone.Start(device, true, 999, 44100);
         micRecord = Microphone.Start(device, true, 999, 44100);
-
-
+        _audioSpectrum = new float[256];
     }
 
     // Update is called once per frame
     void Update()
     {
         volume=GetMaxVolume();
+        //m_MyAudioSource.clip = micRecord;
+        m_MyAudioSource.GetSpectrumData(_audioSpectrum, 0, FFTWindow.Hamming);
+        if (_audioSpectrum != null && _audioSpectrum.Length > 0)
+        {
+            spectrumValue = _audioSpectrum[0] * 100;
+        }
     }
     float GetMaxVolume()
     {
---
+++ Atlandis_v0.0.1(upload)/Assets/Scripts/freqControl.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class freqControl : MonoBehaviour
+{
+    public float audioValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioValue = Mic_Input.spectrumValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
